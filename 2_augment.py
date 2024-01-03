7facc3c22c28b9329871f4c975c3469c00384403
Jie Huang <jiehuang.fi@gmail.com>
2024-01-03 17:18:43 +0200

    not need to pass command line parameters

--- 2_augment.py
+++ 2_augment.py
@@ -1,14 +1,7 @@
 '''
 This script is used to load data, and augment the data using style translation.
 Usage:
-    python 2_augment.py --dataroot image_input --name HyperTest-resnet_6blocks-2-16-10-0.5 \
-       --model test --netG resnet_6blocks  --ngf 16   --no_dropout --input_nc 1 \
-       --output_nc 1  --checkpoints_dir trained_models \
-       --results_dir image_output
-
-    or
-
-    ./run.sh 
+    python 2_augment.py 
 '''
 import os, sys
 sys.path.append( './ASD-AFM-dev')
@@ -25,14 +18,18 @@ import random
 import matplotlib.pyplot as plt
 import yaml
 
+import numpy as np
+
+
 # CycleGAN model
 sys.path.append('./pytorch-CycleGAN-and-pix2pix')
 from options.test_options import TestOptions
 from data import create_dataset
 from models import create_model
 from util.visualizer import save_images
-import numpy as np
+
 import torch
+import argparse
 
 # Make changes in this function to style translation
 def apply_preprocessing(batch, cfg, gen_ab):
@@ -100,8 +97,67 @@ def make_webDataloader(cfg, gen_ab, mode='train'):
 
 def main():
 
-    # Load cycle GAN model
-    opt = TestOptions().parse()  # get options from command line
+    # Load cycle GAN model not from command line parameters
+    parser = argparse.ArgumentParser(description='Test the trained model.')
+    parser.add_argument('--dataroot', default='image_input', help='path to images (should \
+                        have subfolders trainA, trainB, valA, valB, etc)')
+    parser.add_argument('--name', type=str, default='HyperTest-resnet_6blocks-2-16-10-0.5', \
+                        help='name of the experiment. It decides where to store samples and models')
+    parser.add_argument('--gpu_ids', type=str, default='0', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
+    parser.add_argument('--checkpoints_dir', type=str, default='./trained_models', help='models are saved here')
+    parser.add_argument('--model', type=str, default='test', help='chooses which model to use. [cycle_gan | pix2pix | test | colorization]')
+    parser.add_argument('--input_nc', type=int, default=1, help='# of input image channels: 3 for RGB and 1 for grayscale')
+    parser.add_argument('--output_nc', type=int, default=1, help='# of output image channels: 3 for RGB and 1 for grayscale')
+    parser.add_argument('--ngf', type=int, default=16, help='# of gen filters in the last conv layer')
+    parser.add_argument('--ndf', type=int, default=16, help='# of discrim filters in the first conv layer')
+    parser.add_argument('--netD', type=str, default='basic', help='specify discriminator architecture [basic | n_layers | pixel]. The basic model is a 70x70 PatchGAN. n_layers allows you to specify the layers in the discriminator')
+    parser.add_argument('--netG', type=str, default='resnet_6blocks', help='specify generator architecture [resnet_9blocks | resnet_6blocks | unet_256 | unet_128]')
+    parser.add_argument('--n_layers_D', type=int, default=3, help='only used if netD==n_layers')
+    parser.add_argument('--norm', type=str, default='instance', help='instance normalization or batch normalization [instance | batch | none]')
+    parser.add_argument('--init_type', type=str, default='normal', help='network initialization [normal | xavier | kaiming | orthogonal]')
+    parser.add_argument('--init_gain', type=float, default=0.02, help='scaling factor for normal, xavier and orthogonal.')
+    parser.add_argument('--no_dropout', action='store_false', help='no dropout for the generator')
+    # dataset parameters
+    parser.add_argument('--dataset_mode', type=str, default='single', help='chooses how datasets are loaded. [unaligned | aligned | single | colorization]')
+    parser.add_argument('--direction', type=str, default='AtoB', help='AtoB or BtoA')
+    parser.add_argument('--serial_batches', action='store_true', help='if true, takes images in order to make batches, otherwise takes them randomly')
+    parser.add_argument('--num_threads', default=4, type=int, help='# threads for loading data')
+    parser.add_argument('--batch_size', type=int, default=1, help='input batch size')
+    parser.add_argument('--load_size', type=int, default=256, help='scale images to this size')
+    parser.add_argument('--crop_size', type=int, default=256, help='then crop to this size')
+    parser.add_argument('--max_dataset_size', type=int, default=float("inf"), help='Maximum number of samples allowed per dataset. If the dataset directory contains more than max_dataset_size, only a subset is loaded.')
+    parser.add_argument('--preprocess', type=str, default='resize_and_crop', help='scaling and cropping of images at load time [resize_and_crop | crop | scale_width | scale_width_and_crop | none]')
+    parser.add_argument('--no_flip', action='store_true', help='if specified, do not flip the images for data augmentation')
+    parser.add_argument('--display_winsize', type=int, default=256, help='display window size for both visdom and HTML')
+    # additional parameters
+    parser.add_argument('--epoch', type=str, default='latest', help='which epoch to load? set to latest to use latest cached model')
+    parser.add_argument('--load_iter', type=int, default=0, help='which iteration to load? if load_iter > 0, the code will load models by iter_[load_iter]; otherwise, the code will load models by [epoch]')
+    parser.add_argument('--verbose', action='store_true', help='if specified, print more debugging information')
+    parser.add_argument('--suffix', default='', type=str, help='customized suffix: opt.name = opt.name + suffix: e.g., {model}_{netG}_size{load_size}')
+    # wandb parameters
+    parser.add_argument('--use_wandb', action='store_true', help='if specified, then init wandb logging')
+    parser.add_argument('--wandb_project_name', type=str, default='CycleGAN-and-pix2pix', help='specify wandb project name')
+    parser.add_argument('--results_dir', type=str, default='./image_output/', help='saves results here.')
+    parser.add_argument('--aspect_ratio', type=float, default=1.0, help='aspect ratio of result images')
+    parser.add_argument('--phase', type=str, default='test', help='train, val, test, etc')
+    # Dropout and Batchnorm has different behavioir during training and test.
+    parser.add_argument('--eval', action='store_true', help='use eval mode during test time.')
+    parser.add_argument('--num_test', type=int, default=50, help='how many test images to run')
+    parser.add_argument('--model_suffix', type=str, default='', help='')
+    parser.add_argument('--isTrain', action='store_true', help='')
+
+    opt = parser.parse_args()
+
+    # set gpu ids
+    str_ids = opt.gpu_ids.split(',')
+    opt.gpu_ids = []
+    for str_id in str_ids:
+        id = int(str_id)
+        if id >= 0:
+            opt.gpu_ids.append(id)
+    if len(opt.gpu_ids) > 0:
+        torch.cuda.set_device(opt.gpu_ids[0])
+
     opt.num_threads = 0   # test code only supports num_threads = 0
     opt.batch_size = 1    # test code only supports batch_size = 1
     opt.serial_batches = True  # disable data shuffling.
--- 2_run.sh
+++
@@ -1,4 +0,0 @@
-python 2_augment.py --dataroot image_input --name HyperTest-resnet_6blocks-2-16-10-0.5 \
-       --model test --netG resnet_6blocks  --ngf 16   --no_dropout --input_nc 1 \
-       --output_nc 1  --checkpoints_dir trained_models \
-       --results_dir image_output
