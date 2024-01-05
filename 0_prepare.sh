cbd55d481c69534d3cea3a462a09e366676bf306
Jie Huang <jiehuang.fi@gmail.com>
2024-01-05 11:09:26 +0200

    check whether on GPU automatically

--- 0_prepare.sh
+++ 0_prepare.sh
@@ -4,5 +4,10 @@ eval "$(conda shell.bash hook)"
 conda activate ml &&
 conda info | grep "active environment" &&
 
-echo 'Entering GPU node ...'
-srun -p gpushort --gres=gpu:4 --constraint=pascal  --time=4:00:00 --mem=6000M --pty bash
+# If the host is a GPU node (node name contain gpu or dgx), then do nothing, else enter a GPU node
+if [[ $HOSTNAME == *"gpu"* ]] || [[ $HOSTNAME == *"dgx"* ]]; then
+    echo "Already in GPU node $HOSTNAME"
+else
+    echo "Entering GPU node ..."
+    srun -p gpushort --gres=gpu:4 --constraint=pascal  --time=4:00:00 --mem=6000M --pty bash
+fi
