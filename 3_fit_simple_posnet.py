0394c3ccc1f6ce3dce55cb30265a8e6f1b029441
Jie Huang <jiehuang.fi@gmail.com>
2024-01-05 15:10:02 +0200

    reorganize the code to make it clean

--- 3_fit_simple_posnet.py
+++ 3_fit_simple_posnet.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 '''
 This script is used to show fitting the posnet model using cylceGAN data augmentation method.
 '''
@@ -31,8 +33,10 @@ from asdafm.graph.models            import PosNetAdaptive
 from asdafm.parsing_utils           import update_config
 from asdafm.logging                 import LossLogPlot, SyncedLoss
 
-# This is used to load cycle GAN model
 import argparse
+sys.path.append('./pytorch-CycleGAN-and-pix2pix') # Path to cycleGAN repo
+from data import create_dataset
+from models import create_model
 
 def batch_to_device(batch, device):
     X, ref, *rest = batch
@@ -128,17 +132,80 @@ def make_model(device, cfg):
     return model, criterion, optimizer, lr_decay
 
 
-def run(rank, cfg, gen_ab=None):
+def obtain_cycleGAN_options():
+    # Load cycle GAN model options
+    parser = argparse.ArgumentParser(description='Test the trained model.')
+    parser.add_argument('--dataroot', default='image_input', help='path to images (should \
+                        have subfolders trainA, trainB, valA, valB, etc)')
+    parser.add_argument('--name', type=str, default='HyperTest-resnet_6blocks-2-16-10-0.5', \
+                        help='name of the experiment. It decides where to store samples and models')
+    parser.add_argument('--gpu_ids', type=str, default='-1', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
+    parser.add_argument('--checkpoints_dir', type=str, default='./trained_models', help='models are saved here')
+    parser.add_argument('--model', type=str, default='test', help='chooses which model to use. [cycle_gan | pix2pix | test | colorization]')
+    parser.add_argument('--input_nc', type=int, default=1, help='# of input image channels: 3 for RGB and 1 for grayscale')
+    parser.add_argument('--output_nc', type=int, default=1, help='# of output image channels: 3 for RGB and 1 for grayscale')
+    parser.add_argument('--ngf', type=int, default=16, help='# of gen filters in the last conv layer')
+    parser.add_argument('--ndf', type=int, default=16, help='# of discrim filters in the first conv layer')
+    parser.add_argument('--netD', type=str, default='basic', help='specify discriminator architecture [basic | n_layers | pixel]. The basic model is a 70x70 PatchGAN. n_layers allows you to specify the layers in the discriminator')
+    parser.add_argument('--netG', type=str, default='resnet_6blocks', help='specify generator architecture [resnet_9blocks | resnet_6blocks | unet_256 | unet_128]')
+    parser.add_argument('--n_layers_D', type=int, default=3, help='only used if netD==n_layers')
+    parser.add_argument('--norm', type=str, default='instance', help='instance normalization or batch normalization [instance | batch | none]')
+    parser.add_argument('--init_type', type=str, default='normal', help='network initialization [normal | xavier | kaiming | orthogonal]')
+    parser.add_argument('--init_gain', type=float, default=0.02, help='scaling factor for normal, xavier and orthogonal.')
+    parser.add_argument('--no_dropout', action='store_false', help='no dropout for the generator')
+
+    parser.add_argument('--dataset_mode', type=str, default='single', help='chooses how datasets are loaded. [unaligned | aligned | single | colorization]')
+    parser.add_argument('--direction', type=str, default='AtoB', help='AtoB or BtoA')
+    parser.add_argument('--serial_batches', action='store_false', help='if true, takes images in order to make batches, otherwise takes them randomly')
+    parser.add_argument('--num_threads', default=0, type=int, help='# threads for loading data') # test code only supports num_threads = 0
+    parser.add_argument('--batch_size', type=int, default=1, help='input batch size') # test code only supports batch_size = 1
+    parser.add_argument('--load_size', type=int, default=256, help='scale images to this size')
+    parser.add_argument('--crop_size', type=int, default=256, help='then crop to this size')
+    parser.add_argument('--max_dataset_size', type=int, default=float("inf"), help='Maximum number of samples allowed per dataset. If the dataset directory contains more than max_dataset_size, only a subset is loaded.')
+    parser.add_argument('--preprocess', type=str, default='resize_and_crop', help='scaling and cropping of images at load time [resize_and_crop | crop | scale_width | scale_width_and_crop | none]')
+    parser.add_argument('--no_flip', action='store_false', help='if specified, do not flip the images for data augmentation')
+    parser.add_argument('--display_winsize', type=int, default=256, help='display window size for both visdom and HTML')
+
+    parser.add_argument('--epoch', type=str, default='latest', help='which epoch to load? set to latest to use latest cached model')
+    parser.add_argument('--load_iter', type=int, default=0, help='which iteration to load? if load_iter > 0, the code will load models by iter_[load_iter]; otherwise, the code will load models by [epoch]')
+    parser.add_argument('--verbose', action='store_true', help='if specified, print more debugging information')
+    parser.add_argument('--suffix', default='', type=str, help='customized suffix: opt.name = opt.name + suffix: e.g., {model}_{netG}_size{load_size}')
+
+    # wandb parameters
+    parser.add_argument('--use_wandb', action='store_true', help='if specified, then init wandb logging')
+    parser.add_argument('--wandb_project_name', type=str, default='CycleGAN-and-pix2pix', help='specify wandb project name')
+    parser.add_argument('--results_dir', type=str, default='./image_output/', help='saves results here.')
+    parser.add_argument('--aspect_ratio', type=float, default=1.0, help='aspect ratio of result images')
+    parser.add_argument('--phase', type=str, default='test', help='train, val, test, etc')
+
+    # Dropout and Batchnorm has different behavioir during training and test.
+    parser.add_argument('--eval', action='store_true', help='use eval mode during test time.')
+    parser.add_argument('--num_test', type=int, default=50, help='how many test images to run')
+    parser.add_argument('--model_suffix', type=str, default='', help='')
+    parser.add_argument('--isTrain', action='store_true', help='') 
+    parser.add_argument('--display_id', type=int, default=-1, help='input batch size') # no visdom display
+
+    opt = parser.parse_args()
+    return opt
+
+
+def run(rank, cfg):
     # Initialize the distributed environment. (Fixed pattern)
     os.environ['MASTER_ADDR'] = 'localhost'
-    os.environ['MASTER_PORT'] = '12356'
+    os.environ['MASTER_PORT'] = '12355'
     dist.init_process_group(cfg['comm_backend'], rank=rank, world_size=cfg['world_size'])
     cfg['rank'] = rank
     cfg['local_rank'] = rank
     cfg['global_rank'] = rank
 
+    # Load cycle GAN model if style_trans is True
+    if cfg['style_trans'] == True:
+        opt = obtain_cycleGAN_options()
+        opt.gpu_ids = [rank] # use local rank as the device ID
+        gen_ab = create_model(opt) # create a model given opt.model and other options
+        gen_ab.setup(opt)  # regular setup: load and print networks; create schedulers
+    
     start_time = time.perf_counter()  
-
     # Create a directory called Checkpoints in the run_dir ('.')
     checkpoint_dir = os.path.join(cfg['run_dir'], 'Checkpoints/')
     if rank == 0:
@@ -148,8 +215,7 @@ def run(rank, cfg, gen_ab=None):
             os.makedirs(checkpoint_dir)
 
     # Define model, optimizer, and loss
-    model, criterion, optimizer, lr_decay = make_model(rank, cfg)  
-
+    model, criterion, optimizer, lr_decay = make_model(rank, cfg)
     if rank == 0:
         print(f'CUDA is available = {torch.cuda.is_available()}')
         print(f'Model total parameters: {cu.count_parameters(model)}')
@@ -187,9 +253,6 @@ def run(rank, cfg, gen_ab=None):
                 print('Model already trained')
         
         for epoch in range(init_epoch, cfg['epochs']+1):
-            
-            print('Epoch: ', epoch)
-
             if rank == 0: print(f'\n === Epoch {epoch}')
             
             # Train
@@ -364,86 +427,6 @@ if __name__ == '__main__':
     with open('./config_styleTrans.yaml', 'r') as f:
         cfg = yaml.safe_load(f)
 
-    # Load cycle GAN model if style_trans is True
-    if cfg['style_trans'] == True:
-        sys.path.append('./pytorch-CycleGAN-and-pix2pix')
-        from data import create_dataset
-        from models import create_model
-
-        # Load cycle GAN model not from command line parameters
-        parser = argparse.ArgumentParser(description='Test the trained model.')
-        parser.add_argument('--dataroot', default='image_input', help='path to images (should \
-                            have subfolders trainA, trainB, valA, valB, etc)')
-        parser.add_argument('--name', type=str, default='HyperTest-resnet_6blocks-2-16-10-0.5', \
-                            help='name of the experiment. It decides where to store samples and models')
-        parser.add_argument('--gpu_ids', type=str, default='-1', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
-        parser.add_argument('--checkpoints_dir', type=str, default='./trained_models', help='models are saved here')
-        parser.add_argument('--model', type=str, default='test', help='chooses which model to use. [cycle_gan | pix2pix | test | colorization]')
-        parser.add_argument('--input_nc', type=int, default=1, help='# of input image channels: 3 for RGB and 1 for grayscale')
-        parser.add_argument('--output_nc', type=int, default=1, help='# of output image channels: 3 for RGB and 1 for grayscale')
-        parser.add_argument('--ngf', type=int, default=16, help='# of gen filters in the last conv layer')
-        parser.add_argument('--ndf', type=int, default=16, help='# of discrim filters in the first conv layer')
-        parser.add_argument('--netD', type=str, default='basic', help='specify discriminator architecture [basic | n_layers | pixel]. The basic model is a 70x70 PatchGAN. n_layers allows you to specify the layers in the discriminator')
-        parser.add_argument('--netG', type=str, default='resnet_6blocks', help='specify generator architecture [resnet_9blocks | resnet_6blocks | unet_256 | unet_128]')
-        parser.add_argument('--n_layers_D', type=int, default=3, help='only used if netD==n_layers')
-        parser.add_argument('--norm', type=str, default='instance', help='instance normalization or batch normalization [instance | batch | none]')
-        parser.add_argument('--init_type', type=str, default='normal', help='network initialization [normal | xavier | kaiming | orthogonal]')
-        parser.add_argument('--init_gain', type=float, default=0.02, help='scaling factor for normal, xavier and orthogonal.')
-        parser.add_argument('--no_dropout', action='store_false', help='no dropout for the generator')
-
-        parser.add_argument('--dataset_mode', type=str, default='single', help='chooses how datasets are loaded. [unaligned | aligned | single | colorization]')
-        parser.add_argument('--direction', type=str, default='AtoB', help='AtoB or BtoA')
-        parser.add_argument('--serial_batches', action='store_true', help='if true, takes images in order to make batches, otherwise takes them randomly')
-        parser.add_argument('--num_threads', default=4, type=int, help='# threads for loading data')
-        parser.add_argument('--batch_size', type=int, default=1, help='input batch size')
-        parser.add_argument('--load_size', type=int, default=256, help='scale images to this size')
-        parser.add_argument('--crop_size', type=int, default=256, help='then crop to this size')
-        parser.add_argument('--max_dataset_size', type=int, default=float("inf"), help='Maximum number of samples allowed per dataset. If the dataset directory contains more than max_dataset_size, only a subset is loaded.')
-        parser.add_argument('--preprocess', type=str, default='resize_and_crop', help='scaling and cropping of images at load time [resize_and_crop | crop | scale_width | scale_width_and_crop | none]')
-        parser.add_argument('--no_flip', action='store_true', help='if specified, do not flip the images for data augmentation')
-        parser.add_argument('--display_winsize', type=int, default=256, help='display window size for both visdom and HTML')
-
-        parser.add_argument('--epoch', type=str, default='latest', help='which epoch to load? set to latest to use latest cached model')
-        parser.add_argument('--load_iter', type=int, default=0, help='which iteration to load? if load_iter > 0, the code will load models by iter_[load_iter]; otherwise, the code will load models by [epoch]')
-        parser.add_argument('--verbose', action='store_true', help='if specified, print more debugging information')
-        parser.add_argument('--suffix', default='', type=str, help='customized suffix: opt.name = opt.name + suffix: e.g., {model}_{netG}_size{load_size}')
-
-        # wandb parameters
-        parser.add_argument('--use_wandb', action='store_true', help='if specified, then init wandb logging')
-        parser.add_argument('--wandb_project_name', type=str, default='CycleGAN-and-pix2pix', help='specify wandb project name')
-        parser.add_argument('--results_dir', type=str, default='./image_output/', help='saves results here.')
-        parser.add_argument('--aspect_ratio', type=float, default=1.0, help='aspect ratio of result images')
-        parser.add_argument('--phase', type=str, default='test', help='train, val, test, etc')
-
-        # Dropout and Batchnorm has different behavioir during training and test.
-        parser.add_argument('--eval', action='store_true', help='use eval mode during test time.')
-        parser.add_argument('--num_test', type=int, default=50, help='how many test images to run')
-        parser.add_argument('--model_suffix', type=str, default='', help='')
-        parser.add_argument('--isTrain', action='store_true', help='')
-
-        opt = parser.parse_args()
-
-        # set gpu ids
-        str_ids = opt.gpu_ids.split(',')
-        opt.gpu_ids = []
-        for str_id in str_ids:
-            id = int(str_id)
-            if id >= 0:
-                opt.gpu_ids.append(id)
-        if len(opt.gpu_ids) > 0:
-            torch.cuda.set_device(opt.gpu_ids[0])
-
-        # Load cycle GAN model
-        opt.num_threads = 0   # test code only supports num_threads = 0
-        opt.batch_size = 1    # test code only supports batch_size = 1
-        opt.serial_batches = True  # disable data shuffling.
-        opt.no_flip = True    # no flip
-        opt.display_id = -1   # no visdom display
-        gen_ab = create_model(opt) # create a model given opt.model and other options
-        gen_ab.setup(opt)  # regular setup: load and print networks; create schedulers
-        # torch.multiprocessing.set_start_method('spawn') # avoid errors in dataloader ?
-
-
     if not os.path.exists(cfg['run_dir']):
         os.makedirs(cfg['run_dir'])
     # Save config
@@ -453,9 +436,7 @@ if __name__ == '__main__':
     # Set random seeds from config file
     torch.manual_seed(cfg['random_seed'])
     random.seed(cfg['random_seed'])
-    np.random.seed(cfg['random_seed'])   
-    print('Add seeds:')
-    print(cfg)
+    np.random.seed(cfg['random_seed']) 
 
     # Start run
     mp.set_start_method('spawn')
@@ -463,8 +444,5 @@ if __name__ == '__main__':
     cfg['world_size'] = world_size 
 
     # Distributed Data Parallel
-    if cfg['style_trans'] == True:
-        mp.spawn(run, args=(cfg, gen_ab), nprocs=world_size, join=True)
-    else:
-        mp.spawn(run, args=(cfg, ), nprocs=world_size, join=True)
+    mp.spawn(run, args=(cfg, ), nprocs=world_size, join=True)
 
