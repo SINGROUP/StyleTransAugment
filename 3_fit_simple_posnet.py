d978401a6bf4b65998b935fa2996c400e11588a0
Jie Huang <jiehuang.fi@gmail.com>
2024-01-09 15:22:34 +0200

    resolve command line parameter conflict

--- 3_fit_simple_posnet.py
+++ 3_fit_simple_posnet.py
@@ -131,60 +131,63 @@ def make_model(device, cfg):
     return model, criterion, optimizer, lr_decay
 
 
-def obtain_cycleGAN_options():
-    # Load cycle GAN model options
-    parser = argparse.ArgumentParser(description='Test the trained model.')
-    parser.add_argument('--dataroot', default='image_input', help='path to images (should \
-                        have subfolders trainA, trainB, valA, valB, etc)')
-    parser.add_argument('--name', type=str, default='HyperTest-resnet_6blocks-2-16-10-0.5', \
-                        help='name of the experiment. It decides where to store samples and models')
-    parser.add_argument('--gpu_ids', type=str, default='-1', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
-    parser.add_argument('--checkpoints_dir', type=str, default='./trained_models', help='models are saved here')
-    parser.add_argument('--model', type=str, default='test', help='chooses which model to use. [cycle_gan | pix2pix | test | colorization]')
-    parser.add_argument('--input_nc', type=int, default=1, help='# of input image channels: 3 for RGB and 1 for grayscale')
-    parser.add_argument('--output_nc', type=int, default=1, help='# of output image channels: 3 for RGB and 1 for grayscale')
-    parser.add_argument('--ngf', type=int, default=16, help='# of gen filters in the last conv layer')
-    parser.add_argument('--ndf', type=int, default=16, help='# of discrim filters in the first conv layer')
-    parser.add_argument('--netD', type=str, default='basic', help='specify discriminator architecture [basic | n_layers | pixel]. The basic model is a 70x70 PatchGAN. n_layers allows you to specify the layers in the discriminator')
-    parser.add_argument('--netG', type=str, default='resnet_6blocks', help='specify generator architecture [resnet_9blocks | resnet_6blocks | unet_256 | unet_128]')
-    parser.add_argument('--n_layers_D', type=int, default=3, help='only used if netD==n_layers')
-    parser.add_argument('--norm', type=str, default='instance', help='instance normalization or batch normalization [instance | batch | none]')
-    parser.add_argument('--init_type', type=str, default='normal', help='network initialization [normal | xavier | kaiming | orthogonal]')
-    parser.add_argument('--init_gain', type=float, default=0.02, help='scaling factor for normal, xavier and orthogonal.')
-    parser.add_argument('--no_dropout', action='store_false', help='no dropout for the generator')
+def cycleGAN_options(options_dict):
 
-    parser.add_argument('--dataset_mode', type=str, default='single', help='chooses how datasets are loaded. [unaligned | aligned | single | colorization]')
-    parser.add_argument('--direction', type=str, default='AtoB', help='AtoB or BtoA')
-    parser.add_argument('--serial_batches', action='store_false', help='if true, takes images in order to make batches, otherwise takes them randomly')
-    parser.add_argument('--num_threads', default=0, type=int, help='# threads for loading data') # test code only supports num_threads = 0
-    parser.add_argument('--batch_size', type=int, default=1, help='input batch size') # test code only supports batch_size = 1
-    parser.add_argument('--load_size', type=int, default=256, help='scale images to this size')
-    parser.add_argument('--crop_size', type=int, default=256, help='then crop to this size')
-    parser.add_argument('--max_dataset_size', type=int, default=float("inf"), help='Maximum number of samples allowed per dataset. If the dataset directory contains more than max_dataset_size, only a subset is loaded.')
-    parser.add_argument('--preprocess', type=str, default='resize_and_crop', help='scaling and cropping of images at load time [resize_and_crop | crop | scale_width | scale_width_and_crop | none]')
-    parser.add_argument('--no_flip', action='store_false', help='if specified, do not flip the images for data augmentation')
-    parser.add_argument('--display_winsize', type=int, default=256, help='display window size for both visdom and HTML')
+    default_values_dict = {
+        'dataroot': 'image_input',
+        'name': 'HyperTest-resnet_6blocks-2-16-10-0.5',
+        'gpu_ids': '-1',
+        'checkpoints_dir': './trained_models',
+        'model': 'test',
+        'input_nc': 1,
+        'output_nc': 1,
+        'ngf': 16,
+        'ndf': 16,
+        'netD': 'basic',
+        'netG': 'resnet_6blocks',
+        'n_layers_D': 3,
+        'norm': 'instance',
+        'init_type': 'normal',
+        'init_gain': 0.02,
+        'no_dropout': True,
+        'dataset_mode': 'single',
+        'direction': 'AtoB',
+        'serial_batches': True,
+        'num_threads': 0,
+        'batch_size': 1,
+        'load_size': 256, #
+        'crop_size': 256, #
+        'max_dataset_size': float("inf"),
+        'preprocess': 'resize_and_crop', #
+        'no_flip': True,
+        'display_winsize': 256,
+        'epoch': 'latest',
+        'load_iter': 0,
+        'verbose': False,
+        'suffix': '',
+        'use_wandb': False,
+        'wandb_project_name': 'CycleGAN-and-pix2pix',
+        'results_dir': './image_output/',
+        'aspect_ratio': 1.0,
+        'phase': 'test',
+        'eval': False,
+        'num_test': 50,
+        'model_suffix': '',
+        'isTrain': False, 
+        'display_id': -1
+    }
 
-    parser.add_argument('--epoch', type=str, default='latest', help='which epoch to load? set to latest to use latest cached model')
-    parser.add_argument('--load_iter', type=int, default=0, help='which iteration to load? if load_iter > 0, the code will load models by iter_[load_iter]; otherwise, the code will load models by [epoch]')
-    parser.add_argument('--verbose', action='store_true', help='if specified, print more debugging information')
-    parser.add_argument('--suffix', default='', type=str, help='customized suffix: opt.name = opt.name + suffix: e.g., {model}_{netG}_size{load_size}')
+    # Ensure that options_dict only alters known arguments, if not you may raise an error
+    for key in options_dict.keys():
+        if key not in default_values_dict:
+            raise ValueError(f"Unknown option {key}")
 
-    # wandb parameters
-    parser.add_argument('--use_wandb', action='store_true', help='if specified, then init wandb logging')
-    parser.add_argument('--wandb_project_name', type=str, default='CycleGAN-and-pix2pix', help='specify wandb project name')
-    parser.add_argument('--results_dir', type=str, default='./image_output/', help='saves results here.')
-    parser.add_argument('--aspect_ratio', type=float, default=1.0, help='aspect ratio of result images')
-    parser.add_argument('--phase', type=str, default='test', help='train, val, test, etc')
+    # updating default values with options from `options_dict`
+    default_values_dict.update(options_dict)
 
-    # Dropout and Batchnorm has different behavioir during training and test.
-    parser.add_argument('--eval', action='store_true', help='use eval mode during test time.')
-    parser.add_argument('--num_test', type=int, default=50, help='how many test images to run')
-    parser.add_argument('--model_suffix', type=str, default='', help='')
-    parser.add_argument('--isTrain', action='store_true', help='') 
-    parser.add_argument('--display_id', type=int, default=-1, help='input batch size') # no visdom display
+    # Convert dict to Namespace
+    opt = argparse.Namespace(**default_values_dict)
 
-    opt = parser.parse_args()
     return opt
 
 
@@ -197,9 +200,54 @@ def run(rank, cfg):
     cfg['local_rank'] = rank
     cfg['global_rank'] = rank
 
+
+    options_dict = {
+        'dataroot': 'image_input',
+        'name': 'HyperTest-resnet_6blocks-2-16-10-0.5',
+        'gpu_ids': '-1',
+        'checkpoints_dir': './trained_models',
+        'model': 'test',
+        'input_nc': 1,
+        'output_nc': 1,
+        'ngf': 16,
+        'ndf': 16,
+        'netD': 'basic',
+        'netG': 'resnet_6blocks',
+        'n_layers_D': 3,
+        'norm': 'instance',
+        'init_type': 'normal',
+        'init_gain': 0.02,
+        'no_dropout': True,
+        'dataset_mode': 'single',
+        'direction': 'AtoB',
+        'serial_batches': True,
+        'num_threads': 0,
+        'batch_size': 1,
+        'load_size': 256,
+        'crop_size': 256,
+        'max_dataset_size': float("inf"),
+        'preprocess': 'resize_and_crop',
+        'no_flip': True,
+        'display_winsize': 256,
+        'epoch': 'latest',
+        'load_iter': 0,
+        'verbose': False,
+        'suffix': '',
+        'use_wandb': False,
+        'wandb_project_name': 'CycleGAN-and-pix2pix',
+        'results_dir': './image_output/',
+        'aspect_ratio': 1.0,
+        'phase': 'test',
+        'eval': False,
+        'num_test': 50,
+        'model_suffix': '',
+        'isTrain': False, 
+        'display_id': -1
+    }
+
     # Load cycle GAN model if style_trans is True
     if cfg['style_trans'] == True:
-        opt = obtain_cycleGAN_options()
+        opt = cycleGAN_options(options_dict)
         opt.gpu_ids = [rank] # use local rank as the device ID
         gen_ab = create_model(opt) # create a model given opt.model and other options
         gen_ab.setup(opt)  # regular setup: load and print networks; create schedulers
